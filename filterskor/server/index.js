7413a3713cf0629ba41137f5ea8aa6ef6e841703
YouraNoAI <154102639+YouraNoAI@users.noreply.github.com>
2025-07-01 16:45:43 +0000

    Refining the Docker

--- filterskor/server/index.js
+++ filterskor/server/index.js
@@ -10,6 +10,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const PORT2 = 3000
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -19,8 +20,9 @@ app.use('/api', router);
 initDatabase()
   .then(() => {
     console.log('✅ Tables created');
-    app.listen(PORT, () =>
-      console.log(`🚀 Server jalan di http://localhost:${PORT}`)
-    );
+    app.listen(PORT, () => {
+      console.log(`🚀 App codespace berjalan di: https://${process.env.CODESPACE_NAME}-${PORT2}.app.github.dev`);
+      console.log(`🚀 App Local berjalan di localhost:${PORT2}`);
+});
   })
   .catch((err) => console.error('❌ DB Init Error:', err));
